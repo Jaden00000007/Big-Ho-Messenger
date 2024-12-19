e67a0adf1e10a516b1bd83c37927d95db84619c1
Jaden00000007 <146095161+Jaden00000007@users.noreply.github.com>
2024-12-18 22:32:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,53 @@
 fetch("https://jsonplaceholder.typicode.com/users/")
-  .then(response => response.json())
-  .then(json => {
-      for (user of json) {
-          let html = document.createElement("h1")
-          html.innerText = `
-          ID: ${user.id}
-          Name: ${user.name}
-          Username: ${user.username}
-          Email: ${user.email}
-          Phone Number: ${user.phone}\
-          `
-          html.innerHTML +=
-          `Website: <a href = ${user.website}>${user.website}<a>`
-          html.id = "info"
-          document.getElementById("quazi").appendChild(html)
-      }
-      console.log(json)
-  });
+   .then(response => response.json())
+   .then(json => {
+       for (user of json) {
+           let html = document.createElement("h1")
+           html.innerText = `
+           ID: ${user.id}
+           Name: ${user.name}
+           Username: ${user.username}
+           Email: ${user.email}
+           Phone Number: ${user.phone}\
+           `
+           html.innerHTML +=
+           `Website: <a href = ${user.website}>${user.website}<a>`
+           html.id = "info"
+           document.getElementById("quazi").appendChild(html)
+       }
+       console.log(json)
+   });
 
+for(let i = 1; i < 11; i++){
+    fetch("https://jsonplaceholder.typicode.com/posts/" + i)
+    .then(response => response.json())
+    .then(json => {
+            let html = document.createElement("h1")
+            html.innerText = 
+            `Title: ${json.title}
+            ${json.body}`
+            html.id = "info"
+            document.getElementById("posts").appendChild(html)
+            console.log(json)
+    });
+}
 
+for(let i = 1; i < 11; i++){
+    fetch("https://jsonplaceholder.typicode.com/comments/" + i)
+    .then(response => response.json())
+    .then(json => {
+            let html = document.createElement("h1")
+            html.innerText = 
+            `Email: ${json.email}
+            Title: ${json.name}
+            ${json.body}`
+            html.id = "info"
+            document.getElementById("comments").appendChild(html)
+            console.log(json)
+    });
+}
 
-
-fetch("https://jsonplaceholder.typicode.com/posts/")
-.then(response => response.json())
-.then(json => {
-   for (post of json) {
-       let html = document.createElement("h1")
-       html.innerText =
-       `Title: ${post.title}
-       ${post.body}`
-       html.id = "info"
-       document.getElementById("posts").appendChild(html)
-   }
-   console.log(json)
-});
-
-
-fetch("https://jsonplaceholder.typicode.com/comments/")
-.then(response => response.json())
-.then(json => {
-   for (comment of json) {
-       let html = document.createElement("h1")
-       html.innerText =
-       `Email: ${comment.email}
-       Title: ${comment.name}
-       ${post.body}`
-       html.id = "info"
-       document.getElementById("comments").appendChild(html)
-   }
-   console.log(json)
-});
-
-
-alert()
+let user = prompt("Please enter your skibidi profile name");
+document.getElementById("title").innerHTML(user)
+let i = 0
+localStorage.setItem(i, user)
